583cc5ce96ecd96a1ffe1d461088b2bb49e75a62
Abhinav Mahalley <abhinavmahalley22@gmail.com>
2021-10-03 21:48:56 +0530

    portfolio file added

---
+++ portfolio website/js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+
+// ////////////
+$(window).bind("pageshow", function() {
+  var form = $('form'); 
+  // let the browser natively reset defaults
+  form[0].reset();
+});
